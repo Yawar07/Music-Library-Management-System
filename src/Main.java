41a164ad701f9dc46c38d25cd910a31e40e352e1
Yawar <yawarwani.263@gmail.com>
2025-01-07 16:16:11 +0530

    V 1.0.1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import ui.MenuUI;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome");
         MenuUI menuUI = new MenuUI();
         menuUI.displayMenu();
     }
